380154f805f91cf216d2da4d9e2766d88f8f9269
boodscode237 <65740750+boodscode237@users.noreply.github.com>
2022-01-18 19:23:58 +0500

    Add project files.

---
+++ AccessModifierDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifierDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.SavePerson();
+            Console.ReadLine();
+        }
+    }
+
+    public class ModifiedDataAccess : DataAccess
+    {
+        public void GetUnsecuredconnectionInfos()
+        {
+            GetConnectionString();
+        }
+    }
+    public class CEO : Manager
+    {
+        public void getConnectonInfo()
+        {
+            DataAccess data = new DataAccess();         
+            //formerLastName = "test";
+        }
+    }
+}
---
+++ DemoLibrary/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class DataAccess
+    {
+        protected internal string GetConnectionString()
+        {
+            return "sensitives data";
+        }
+    }
+    public class Person
+    {
+        private protected string formerLastName = "";
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private string _ssn;
+
+        public string SNN
+        {
+            get { return $"***-**-{_ssn.Substring(_ssn.Length - 4)}"; }
+            set { _ssn = value; }
+        }
+
+        public void ChangeLastName(string newLastName)
+        {
+            formerLastName = LastName;
+            LastName = newLastName;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hello {FirstName}");
+        }
+
+        public void SavePerson()
+        {
+            DataAccess data = new DataAccess();
+            string conn = data.GetConnectionString();
+            // saves a person
+
+        }
+    }
+
+    public class Employee : Person
+    {
+        public string GetFormerLastName()
+        {
+            return formerLastName;
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public string GetAllNames()
+        {
+            return $"{FirstName}, {LastName}, {formerLastName}";
+        }
+    }
+}
+
